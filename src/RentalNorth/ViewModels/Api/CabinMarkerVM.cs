9452252aa65b142fc5a8e41fc456a8dfb1165826
jonas889 <jonas@meljoner.se>
2016-04-13 10:41:40 +0200

    Modeller och dbset för stugor

---
+++ src/RentalNorth/ViewModels/Api/CabinMarkerVM.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RentalNorth.ViewModels.Api
+{
+    public class CabinMarkerVM
+    {
+        // "title": 'Sanjay Gandhi National Park',
+        //"lat": '19.2147067',
+        //"lng": '72.91062020000004',
+        //"description": 'Sanjay Gandhi National Park is a large protected area in the northern part of Mumbai city.'
+        public string Title { get; set; }
+        public double Lat { get; set; }
+        public double lng { get; set; }
+        public string InfoWindow { get; set; }
+    }
+}
